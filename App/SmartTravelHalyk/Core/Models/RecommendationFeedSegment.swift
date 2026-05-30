19844d889f19fe731765675600f1b4b49dc6fefb
Artem Bagin <execoq@gmail.com>
2026-05-31 02:53:49 +0500

    Removed unused segments

--- App/SmartTravelHalyk/Core/Models/RecommendationFeedSegment.swift
+++ App/SmartTravelHalyk/Core/Models/RecommendationFeedSegment.swift
@@ -1,11 +1,9 @@
 import Foundation
 
 enum RecommendationFeedSegment: String, CaseIterable, Identifiable {
-    case forYou
     case similar
     case newStyle
     case seasonal
-    case cashback
 
     var id: String { rawValue }
 }
